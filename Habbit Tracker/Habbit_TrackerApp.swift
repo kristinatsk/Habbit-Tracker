3991fd52d701334170e19647796573174a4c4748
kristinatsk <kristinatsk@gmail.com>
2025-02-09 22:40:22 +0300

    Initial Commit

---
+++ Habbit Tracker/Habbit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habbit_TrackerApp.swift
+//  Habbit Tracker
+//
+//  Created by Kristina Kostenko on 09.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Habbit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
